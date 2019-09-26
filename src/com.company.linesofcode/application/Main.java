1eaf5ff3188d3768b288d22847d98065d01f835c
Henning Wuehn <hwuehn@local>
2019-09-26 13:13:15 +0200

    Feature: added content in Main class, added main method -> new object -> output

--- src/com.company.linesofcode/application/Main.java
+++ src/com.company.linesofcode/application/Main.java
@@ -1,6 +1,16 @@
 package application;
 
+import Data.CodeExample;
+
 public class Main {
+
+    public static void main(String[] args) {
+        CodeExample source = new CodeExample();
+        source.linesPrinted();
+    }
+
+
+
 
 
 }
